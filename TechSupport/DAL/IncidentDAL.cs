f30fe1494ff159207b7befd1fc5cb280099d6cb8
Brandon Drick <bd00051@my.westga.edu>
2022-01-20 16:43:50 -0500

    Created Incident model class and DAL to keep track of incident reports

---
+++ TechSupport/DAL/IncidentDAL.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using TechSupport.Model;
+
+namespace TechSupport.DAL
+{
+    /// <summary>
+    /// Data Access Layer (DAL) for incidents submitted by user.
+    /// </summary>
+    public class IncidentDAL
+    {
+        /// <summary>
+        /// List containing Incident objects.
+        /// </summary>
+        private static List<Incident> _incidents = new List<Incident>
+        {
+            //Default incidents
+            new Incident("Lost Password", "none", 34)
+        };
+    }
+}
---
+++ TechSupport/Model/Incident.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechSupport.Model
+{
+    /// <summary>
+    /// This class models a Tech Support Incident Report.
+    /// </summary>
+    public class Incident
+    {
+        public string Title { get; }
+        public string Description { get; }
+        public int CustomerID { get; }
+
+        public Incident(string title, string description, int customerid)
+        {
+            if (string.IsNullOrEmpty(title))
+            {
+                throw new ArgumentException("Incident title cannot be null or empty", "title");
+            }
+            if (string.IsNullOrEmpty(description))
+            {
+                throw new ArgumentException("Incident description cannot be null or empty", "description");
+            }
+            if (customerid < 1)
+            {
+                throw new ArgumentException("Invalid CustomerID", "customerid");
+            }
+
+            this.Title = title;
+            this.Description = description;
+            this.CustomerID = customerid;
+        }
+    }
+}
